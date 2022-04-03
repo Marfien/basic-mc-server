af3e6f9dcef6a4bee54025e9b0fa1723b5d03984
Marfien <marvin.sempa.haase@gmail.com>
2022-04-03 16:20:22 +0200

    Removed data and dataholder

--- src/main/java/dev/neodym/limbo/data/Data.java
+++
@@ -1,9 +0,0 @@
-package dev.neodym.limbo.data;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface Data {
-
-  @NotNull DataHolder<? extends Data> holder();
-
-}
--- src/main/java/dev/neodym/limbo/data/DataHolder.java
+++
@@ -1,9 +0,0 @@
-package dev.neodym.limbo.data;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface DataHolder<T extends Data> {
-
-  @NotNull Data data();
-
-}
